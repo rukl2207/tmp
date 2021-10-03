c3b580b7627953fdd6840d32977e6d61f75260c7
rus <rus@bk.ru>
2021-10-04 00:18:27 +0300

    add module strings and function multi

--- first/nums.py
+++ first/nums.py
@@ -2,12 +2,15 @@ def plus(a, b):
     return a + b
 
 
-def minus (a, b):
+def minus(a, b):
     return a - b
 
 
-text = "new text"
+def multi(a, b):
+    return a * b
+
 
 if __name__ == '__main__':
     print(plus(2, 3))
     print(minus(5, 2))
+    print(multi(4, 4))
---
+++ first/strings.py
@@ -0,0 +1,6 @@
+def upper_text(str):
+    return str.upper()
+
+
+if __name__ == '__main__':
+    print(upper_text("Hello!"))
