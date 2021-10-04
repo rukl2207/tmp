d6e342806380628d9516552bd63bd1025fb55017
rus <rus@bk.ru>
2021-10-04 11:04:59 +0300

    add conc

--- first/strings.py
+++ first/strings.py
@@ -2,5 +2,10 @@ def upper_text(str):
     return str.upper()
 
 
+def conc(str1, str2):
+    return str1 + str2
+
+
 if __name__ == '__main__':
     print(upper_text("Hello!"))
+    print(conc('Hello ', 'world!'))
